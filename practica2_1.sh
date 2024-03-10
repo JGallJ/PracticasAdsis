9a142eb2b68471d830cab354998bb2eda1300d98
JGallJ <162924316+JGallJ@users.noreply.github.com>
2024-03-10 23:30:31 +0100

    Update practica2_1.sh

--- practica2_1.sh
+++ practica2_1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#868801, Gallardo Jaso , Jorge, [T], [1], [A]
-#869402, Baldovin Cotela, Enrique, [T], [1], [A]
+#868801, Gallardo Jaso, Jorge, T, 1, A
+#869402, Baldovin Cotela, Enrique, T, 1, A
 
 echo -n "Introduzca el nombre del fichero: "
 
